701afd681efdab7d36dc093804252ae34d3fd994
Afila <apidongblog@gmail.com>
2023-03-15 15:22:38 +0700

    tambahkan config_id

--- app/Http/Controllers/Api/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Api/Auth/PasswordResetLinkController.php
@@ -25,9 +25,8 @@ class PasswordResetLinkController extends Controller
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::broker('mandiri')->sendResetLink(
-            $request->only('email')
-        );
+        $credentials = array_merge($request->only('email'), ['config_id' => identitas('id')]);
+        $status = Password::broker('mandiri')->sendResetLink($credentials);
 
         return $status == Password::RESET_LINK_SENT
             ? $this->response(__($status), 200)
