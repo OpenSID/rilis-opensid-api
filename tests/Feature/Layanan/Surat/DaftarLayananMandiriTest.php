c02d8f474951242497f7e32e3340cf10e27ca7b3
apidong <apidong@users.noreply.github.com>
2023-12-09 20:24:53 +0000

    [ci skip] Fix styling

--- tests/Feature/Layanan/Surat/DaftarLayananMandiriTest.php
+++ tests/Feature/Layanan/Surat/DaftarLayananMandiriTest.php
@@ -2,8 +2,6 @@
 
 namespace Tests\Feature\Layanan\Surat;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class DaftarLayananMandiriTest extends TestCase
