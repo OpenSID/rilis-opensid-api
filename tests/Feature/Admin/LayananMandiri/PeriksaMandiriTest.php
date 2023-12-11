e69eabec3e251af00292babc1d668c9dc99519b7
apidong <apidong@users.noreply.github.com>
2023-12-11 07:18:44 +0000

    [ci skip] Fix styling

--- tests/Feature/Admin/LayananMandiri/PeriksaMandiriTest.php
+++ tests/Feature/Admin/LayananMandiri/PeriksaMandiriTest.php
@@ -2,8 +2,6 @@
 
 namespace Tests\Feature\Api\Admin\LayananMandiri;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PeriksaMandiriTest extends TestCase
