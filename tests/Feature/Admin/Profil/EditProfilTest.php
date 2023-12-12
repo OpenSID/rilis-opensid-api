ba811eb5eacd9efc532d88a7829c737272731c7d
Afila <apidongblog@gmail.com>
2023-12-13 02:02:49 +0700

    Update EditProfilTest.php

--- tests/Feature/Admin/Profil/EditProfilTest.php
+++ tests/Feature/Admin/Profil/EditProfilTest.php
@@ -2,21 +2,71 @@
 
 namespace Tests\Feature\Admin\Surat;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class EditProfilTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+
     public function test_updateData()
     {
-        $response = $this->post('/api/admin/profil');
-
+        // test berhasil
+        $this->Admin_user();
+        $data = [
+            'email' => 'afila@gmail.com',
+            'nama' => 'afila'
+        ];
+        $response = $this->put('/api/admin/profil/update', $data , ['Authorization' => "Bearer $this->token"]);
         $response->assertStatus(200);
+
+        // test email tidak lengkap
+        $data = [
+            'email' => 'afila@gm',
+            'nama' => 'afila'
+        ];
+        $response = $this->put('/api/admin/profil/update', $data , ['Authorization' => "Bearer $this->token"]);
+        $response->assertStatus(200);
+
+        // test gagal kurang field
+        $data = [
+            'email' => 'afila@gmail.com',
+        ];
+        $response = $this->put('/api/admin/profil/update', $data , ['Authorization' => "Bearer $this->token"]);
+        $response->assertStatus(301);
+
+        $data = [
+            'nama' => 'afila',
+        ];
+        $response = $this->put('/api/admin/profil/update', $data , ['Authorization' => "Bearer $this->token"]);
+        $response->assertStatus(301);
+    }
+
+    public function test_updatePassword()
+    {
+        $this->Admin_user();
+        $data = [
+            'lama' => '111111',
+            'pass_baru' => 'afila',
+            'pass_baru1' => 'afila',
+        ];
+        $response = $this->put('/api/admin/profil/ganti_password', $data , ['Authorization' => "Bearer $this->token"]);
+        $response->assertStatus(200);
+
+        // test gagal pass baru tidak sama dengan pass baru1
+        $data = [
+            'lama' => '111111',
+            'pass_baru' => 'afila',
+            'pass_baru1' => 'sid304',
+        ];
+        $response = $this->put('/api/admin/profil/ganti_password', $data , ['Authorization' => "Bearer $this->token"]);
+        $response->assertStatus(301);
+
+         // test gagal pass lama salah
+         $data = [
+            'lama' => '123456',
+            'pass_baru' => 'afila',
+            'pass_baru1' => 'sid304',
+        ];
+        $response = $this->put('/api/admin/profil/ganti_password', $data , ['Authorization' => "Bearer $this->token"]);
+        $response->assertStatus(301);
     }
 }
