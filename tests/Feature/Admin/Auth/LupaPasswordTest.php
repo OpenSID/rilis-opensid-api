8f00f03bb94c34206b187f83d83b0ef9ef6d7113
apidong <apidong@users.noreply.github.com>
2023-12-12 19:40:27 +0000

    [ci skip] Fix styling

--- tests/Feature/Admin/Auth/LupaPasswordTest.php
+++ tests/Feature/Admin/Auth/LupaPasswordTest.php
@@ -2,8 +2,6 @@
 
 namespace Tests\Feature\Api\Admin\Auth;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class LupaPasswordTest extends TestCase
@@ -19,21 +17,21 @@ class LupaPasswordTest extends TestCase
         $data = [
             'email' => 'afila@gmail.com',
         ];
-        $response = $this->post('/api/admin/auth/forgot', $data );
+        $response = $this->post('/api/admin/auth/forgot', $data);
         $response->assertStatus(200);
 
         // test gagal email tidak terdaftar di database
         $data = [
             'email' => 'afila22@gmail.com',
         ];
-        $response = $this->post('/api/admin/auth/forgot', $data );
+        $response = $this->post('/api/admin/auth/forgot', $data);
         $response->assertStatus(301);
 
-         // test gagal format email salah
-         $data = [
-            'email' => 'afila22@gm',
+        // test gagal format email salah
+        $data = [
+           'email' => 'afila22@gm',
         ];
-        $response = $this->post('/api/admin/auth/forgot', $data );
+        $response = $this->post('/api/admin/auth/forgot', $data);
         $response->assertStatus(301);
     }
 }
