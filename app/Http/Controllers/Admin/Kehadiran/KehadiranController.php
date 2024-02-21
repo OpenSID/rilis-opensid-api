c4b1b8f164690c66ab72ea64a234b8830463f32d
apidong <apidong@users.noreply.github.com>
2024-02-21 18:56:04 +0000

    [ci skip] Fix styling

--- app/Http/Controllers/Admin/Kehadiran/KehadiranController.php
+++ app/Http/Controllers/Admin/Kehadiran/KehadiranController.php
@@ -18,35 +18,35 @@ class KehadiranController extends BaseController
      */
     public function filter()
     {
-         $ranges = [
-            [
-                'label' =>  'Hari Ini',
-                'value' => [Carbon::now()->format('Y-m-d'), Carbon::now()->format('Y-m-d')]
-            ],
-            [
-                'label' => 'Kemarin',
-                'value' => [Carbon::yesterday()->format('Y-m-d'), Carbon::yesterday()->format('Y-m-d')]
-            ],
-            [
-                'label' => '7 Hari Terakhir',
-                'value' => [Carbon::now()->subDays(6)->format('Y-m-d'), Carbon::now()->format('Y-m-d')]
-            ],
-            [
-                'label' => '30 Hari Terakhir',
-                'value' => [Carbon::now()->subDays(29)->format('Y-m-d'), Carbon::now()->format('Y-m-d')]
-            ],
-            [
-                'label' => 'Bulan Ini',
-                'value' => [Carbon::now()->startOfMonth()->format('Y-m-d'), Carbon::now()->endOfMonth()->format('Y-m-d')]
-            ],
-            [
-                'label' => 'Bulan Lalu',
-                'value' => [Carbon::now()->subMonth()->startOfMonth()->format('Y-m-d'), Carbon::now()->subMonth()->endOfMonth()->format('Y-m-d')]
-            ],
-            [
-                'label' => 'Tahun Ini',
-                'value' =>[Carbon::now()->startOfYear()->format('Y-m-d'), Carbon::now()->endOfYear()->format('Y-m-d')],
-            ],
+        $ranges = [
+           [
+               'label' =>  'Hari Ini',
+               'value' => [Carbon::now()->format('Y-m-d'), Carbon::now()->format('Y-m-d')]
+           ],
+           [
+               'label' => 'Kemarin',
+               'value' => [Carbon::yesterday()->format('Y-m-d'), Carbon::yesterday()->format('Y-m-d')]
+           ],
+           [
+               'label' => '7 Hari Terakhir',
+               'value' => [Carbon::now()->subDays(6)->format('Y-m-d'), Carbon::now()->format('Y-m-d')]
+           ],
+           [
+               'label' => '30 Hari Terakhir',
+               'value' => [Carbon::now()->subDays(29)->format('Y-m-d'), Carbon::now()->format('Y-m-d')]
+           ],
+           [
+               'label' => 'Bulan Ini',
+               'value' => [Carbon::now()->startOfMonth()->format('Y-m-d'), Carbon::now()->endOfMonth()->format('Y-m-d')]
+           ],
+           [
+               'label' => 'Bulan Lalu',
+               'value' => [Carbon::now()->subMonth()->startOfMonth()->format('Y-m-d'), Carbon::now()->subMonth()->endOfMonth()->format('Y-m-d')]
+           ],
+           [
+               'label' => 'Tahun Ini',
+               'value' => [Carbon::now()->startOfYear()->format('Y-m-d'), Carbon::now()->endOfYear()->format('Y-m-d')],
+           ],
         ];
         $pamong = Pamong::daftar()->get();
         $status = Kehadiran::select('status_kehadiran')->groupBy('status_kehadiran')->get();
