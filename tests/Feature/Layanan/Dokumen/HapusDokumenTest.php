520c92971b8d0a4d1cd54dd0c065d1a059231fe1
apidong <apidong@users.noreply.github.com>
2024-04-29 14:04:46 +0000

    [ci skip] Fix styling

--- tests/Feature/Layanan/Dokumen/HapusDokumenTest.php
+++ tests/Feature/Layanan/Dokumen/HapusDokumenTest.php
@@ -22,7 +22,7 @@ class HapusDokumenTest extends TestCase
     {
         $this->Penduduk();
         $id = Dokumen::where('id_pend', 20)->first()->id;
-        
+
         $response = $this->delete("api/v1/layanan-mandiri/dokumen/{$id}/delete", [], [
             'Authorization' => "Bearer $this->token",
             'Accept' => 'application/json',
