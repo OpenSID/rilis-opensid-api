37d939b35792000e589319e343d7de38c8dc06c6
Firliani Fauziah <firlianif@gmail.com>
2024-05-01 13:28:40 +0700

    Rilis v2405.0.0 (#354)
    * Update TambahDokumenTest.php
    
    * [ci skip] Fix styling
    
    * ok
    
    * [ci skip] Fix styling
    
    * [ci skip] memutahirkan catatan rilis
    
    * [ci skip] memutahirkan catatan rilis
    
    * pengaduan
    
    * [ci skip] Fix styling
    
    * ok
    
    * [ci skip] Fix styling
    
    * notifikasi
    
    * [ci skip] Fix styling
    
    * Update PengaduanController.php
    
    * [ci skip] memutahirkan catatan rilis
    
    * hapus dokumen
    
    * [ci skip] Fix styling
    
    * ok
    
    * [ci skip] Fix styling
    
    * Create PesanTest.php
    
    * refaktor
    
    * Update PesanTest.php
    
    * [ci skip] Fix styling
    
    * ok
    
    * [ci skip] Fix styling
    
    * ok
    
    * Update admin.php
    
    * ok
    
    * Update PengaduanEntity.php
    
    * [ci skip] Fix styling
    
    * [ci skip] memutahirkan catatan rilis
    
    * [ci skip] memutahirkan catatan rilis
    
    * [ci skip] memutahirkan catatan rilis
    
    * test
    
    * [ci skip] Fix styling
    
    * Update PengaduanTest.php
    
    * [ci skip] Fix styling
    
    * [ci skip] memutahirkan catatan rilis
    
    * [ci skip] memutahirkan catatan rilis
    
    * mutakhirkan catatan rilis
    
    * ok
    
    * Update catatan_rilis.md
    
    ---------
    
    Co-authored-by: Afila <apidongblog@gmail.com>
    Co-authored-by: apidong <apidong@users.noreply.github.com>
    Co-authored-by: Abah Roland <v.rolanda92@gmail.com>
    Co-authored-by: Abah Roland <59082428+vickyrolanda@users.noreply.github.com>
    Co-authored-by: FirlianiF <FirlianiF@users.noreply.github.com>

---
+++ tests/Feature/Layanan/Dokumen/HapusDokumenTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature\Layanan\Dokumen;
+
+use App\Models\Dokumen;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class HapusDokumenTest extends TestCase
+{
+    // use RefreshDatabase;
+    use DatabaseTransactions;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->beginDatabaseTransaction();
+    }
+
+    public function test_hapus()
+    {
+        $this->Penduduk();
+        $id = Dokumen::where('id_pend', 20)->first()->id;
+
+        $response = $this->delete("api/v1/layanan-mandiri/dokumen/{$id}/delete", [], [
+            'Authorization' => "Bearer $this->token",
+            'Accept' => 'application/json',
+        ]);
+
+        $response->assertStatus(200);
+    }
+}
