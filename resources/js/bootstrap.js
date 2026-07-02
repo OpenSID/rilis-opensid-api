fc54587fd7a19d05d43544ccd7e71d28587ec6ca
Ahmad Affandi <caspianp98@yahoo.com>
2026-07-02 17:22:11 +0700

    rilis 2607

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,13 +1,15 @@
-window._ = require('lodash');
+import _ from 'lodash';
+import axios from 'axios';
+
+window._ = _;
 
 /**
- * We'll load the axios HTTP library which allows us to easily issue requests
+ * We'll configure the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
-
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
@@ -18,11 +20,12 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // import Echo from 'laravel-echo';
 
-// window.Pusher = require('pusher-js');
+// import Pusher from 'pusher-js';
+// window.Pusher = Pusher;
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
